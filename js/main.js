ee91850405426137deb92b88a9c600ca07f374ec
stas-werther <botnarstasyan112002@gmail.com>
2023-03-03 22:58:20 +0200

    commit
    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const swiper = new Swiper('.channel-slider', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 6,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.channel-button-next',
-      prevEl: '.channel-button-prev',
-    },
-  });
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
